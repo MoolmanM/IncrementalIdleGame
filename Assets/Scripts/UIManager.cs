d833cd66746b46b4d63fd46b7c9b705c3884927a
Marius <mmoolman067@gmail.com>
2021-01-05 18:09:47 +0200

    Created new Grid Layout group
    Replaced Vertical layout group with grid layout group. Also added transition to make the whole thing fullscreen.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,19 +6,38 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    public GameObject buildingMainPanel, craftingMainPanel, headerText;
+    public GameObject buildingMainPanel, craftingMainPanel, headerText, buildPanel, layoutGroup;
     public Swipe swipeControls;
     private int menuValue = 0;
+    private float buildPanelY, buildPanelSizeDeltaY;
     public void Start()
     {
         BuildingsTabActive();
+        buildPanelY = buildPanel.transform.localPosition.y;
+        buildPanelSizeDeltaY = buildPanel.GetComponent<RectTransform>().sizeDelta.y;
     }
 
+    public void LargeBuildingTabActive()
+    {
+        buildingMainPanel.SetActive(true);
+        craftingMainPanel.SetActive(false);
+        headerText.GetComponent<TextMeshProUGUI>().text = "Buildings:";
+        buildPanel.transform.localPosition = new Vector2(-0.2293701f, buildPanelY);
+        buildPanel.GetComponent<RectTransform>().sizeDelta = new Vector2(-0.5004883f, buildPanelSizeDeltaY);
+        buildPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(-0.2293701f, buildPanelY);
+        layoutGroup.GetComponent<GridLayoutGroup>().constraintCount = 2;
+        layoutGroup.GetComponent<GridLayoutGroup>().cellSize = new Vector2(540, 80);
+    }
     public void BuildingsTabActive()
     {
         buildingMainPanel.SetActive(true);
         craftingMainPanel.SetActive(false);
         headerText.GetComponent<TextMeshProUGUI>().text = "Buildings:";
+        buildPanel.transform.localPosition = new Vector2(158, -37.1001f);
+        buildPanel.GetComponent<RectTransform>().sizeDelta = new Vector2(-316.9592f, -74.12451f);
+        buildPanel.GetComponent<RectTransform>().anchoredPosition = new Vector2(158, -37.1001f);
+        layoutGroup.GetComponent<GridLayoutGroup>().constraintCount = 1;
+        layoutGroup.GetComponent<GridLayoutGroup>().cellSize = new Vector2(763, 80);
     }
     public void CraftingTabActive()
     {
@@ -28,9 +47,9 @@ public class UIManager : MonoBehaviour
     }
     private void Swiping()
     {
-        if (menuValue < 0)
+        if (menuValue < -1)
         {
-            menuValue = 0;
+            menuValue = -1;
         }
         if (menuValue > 1)
         {
@@ -49,6 +68,10 @@ public class UIManager : MonoBehaviour
     private void Update()
     {
         Swiping();
+        if (menuValue == -1)
+        {
+            LargeBuildingTabActive();
+        }
         if (menuValue == 0)
         {
             BuildingsTabActive();          
